47eb991dd29c4a4acd05172038efb0f3b0a4803e
Danfeik <segura.tinajero.19121@itsmante.edu.mx>
2023-04-28 18:24:53 -0600

    Refactor code

--- src/graphql/student.ts
+++ src/graphql/student.ts
@@ -1,165 +1,171 @@
 import { Student, Gender, StudentState } from "@prisma/client";
 import { GraphQLContext } from "../context";
 
-export const typeDefs =/* GraphQL */ `
-   scalar Date
-    type Student{
-       control: ID! 
-       name: String
-       lastname1:String
-       lastname2:String
-       gender:Gender
-       email:String
-       address_id:Int
-       major_id:Int
-       study_plan_id:Int
-       semester:Int
-       birthday:String
-       student_state:StudentState
-       tutor_id:Int
-    }
-    enum Gender {
-  MALE
-  FEMALE
-  OTHER
-}
-enum StudentState {
-  ACTIVE
-  TEMP_LEAVE
-  OTHER
-}
+export const typeDefs = /* GraphQL */ `
+  scalar Date
+  type Student {
+    control: ID!
+    name: String
+    lastname1: String
+    lastname2: String
+    gender: Gender
+    email: String
+    address_id: Int
+    major_id: Int
+    study_plan_id: Int
+    semester: Int
+    birthday: String
+    student_state: StudentState
+    tutor_id: Int
+  }
+  enum Gender {
+    MALE
+    FEMALE
+    OTHER
+  }
+  enum StudentState {
+    ACTIVE
+    TEMP_LEAVE
+    OTHER
+  }
 
-extend type Query{
-    getAllStudent:[Student]
-}
+  extend type Query {
+    getAllStudent: [Student]
+  }
 
-extend type Mutation{
+  extend type Mutation {
     createStudent(
-        control:String
-       name: String,
-       lastname1:String,
-       lastname2:String,
-       gender:Gender,
-       email:String,
-       address_id:Int,
-       major_id:Int,
-       study_plan_id:Int,
-       semester:Int,
-       birthday:String,
-       student_state:StudentState,
-       tutor_id:Int
-    ):Student
-    deleteStudent(
-        control: String
-    ):Student
-    updateStudent( 
-        control: String, 
-       name: String,
-       lastname1:String,
-       lastname2:String,
-       gender:Gender,
-       email:String,
-       address_id:Int,
-       major_id:Int,
-       study_plan_id:Int,
-       semester:Int,
-       birthday:String,
-       student_state:StudentState,
-       tutor_id:Int):Student
-
-
-}
-
+      control: String
+      name: String
+      lastname1: String
+      lastname2: String
+      gender: Gender
+      email: String
+      address_id: Int
+      major_id: Int
+      study_plan_id: Int
+      semester: Int
+      birthday: String
+      student_state: StudentState
+      tutor_id: Int
+    ): Student
+    deleteStudent(control: String): Student
+    updateStudent(
+      control: String
+      name: String
+      lastname1: String
+      lastname2: String
+      gender: Gender
+      email: String
+      address_id: Int
+      major_id: Int
+      study_plan_id: Int
+      semester: Int
+      birthday: String
+      student_state: StudentState
+      tutor_id: Int
+    ): Student
+  }
 `;
 
 export const resolvers = {
-    Query: {
-        getAllStudent: async (parent: Student,
-            args: {},
-            context: GraphQLContext) => {
-            return context.prisma.student.findMany();
-        }
+  Query: {
+    getAllStudent: async (
+      parent: Student,
+      args: {},
+      context: GraphQLContext
+    ) => {
+      return context.prisma.student.findMany();
     },
-    Mutation: {
-        createStudent: async (parent: Student,
-            args: {
-                control: string;
-                name: string;
-                lastname1: string;
-                lastname2: string;
-                gender: Gender;
-                email: string;
-                address_id: number;
-                major_id: number;
-                study_plan_id: number;
-                semester: number;
-                birthday: string;
-                student_state: StudentState;
-                tutor_id: number
-            },
-            context: GraphQLContext
-        ) => {
-            return context.prisma.student.create({
-                data: {
-                    control: args.control,
-                    name: args.name,
-                    lastname1: args.lastname1,
-                    lastname2: args.lastname2,
-                    gender: args.gender,
-                    email: args.email,
-                    address_id: args.address_id,
-                    major_id: args.major_id,
-                    study_plan_id: args.study_plan_id,
-                    semester: args.semester,
-                    birthday: new Date(args.birthday),
-                    student_state: args.student_state,
-                    tutor_id: args.tutor_id
-                }
-            })
+  },
+  Mutation: {
+    createStudent: async (
+      parent: Student,
+      args: {
+        control: string;
+        name: string;
+        lastname1: string;
+        lastname2: string;
+        gender: Gender;
+        email: string;
+        address_id: number;
+        major_id: number;
+        study_plan_id: number;
+        semester: number;
+        birthday: string;
+        student_state: StudentState;
+        tutor_id: number;
+      },
+      context: GraphQLContext
+    ) => {
+      return context.prisma.student.create({
+        data: {
+          control: args.control,
+          name: args.name,
+          lastname1: args.lastname1,
+          lastname2: args.lastname2,
+          gender: args.gender,
+          email: args.email,
+          address_id: args.address_id,
+          major_id: args.major_id,
+          study_plan_id: args.study_plan_id,
+          semester: args.semester,
+          birthday: new Date(args.birthday),
+          student_state: args.student_state,
+          tutor_id: args.tutor_id,
         },
-        deleteStudent:async (parent:Student, args:{control:string},
-            context:GraphQLContext) => {
-            return context.prisma.student.delete({
-                where:{
-                    control:args.control,
-                }
-            })
-            
+      });
+    },
+    deleteStudent: async (
+      parent: Student,
+      args: { control: string },
+      context: GraphQLContext
+    ) => {
+      return context.prisma.student.delete({
+        where: {
+          control: args.control,
         },
-        updateStudent:async (parent:Student, args:{ 
-            control: string;
-            name: string;
-            lastname1: string;
-            lastname2: string;
-            gender: Gender;
-            email: string;
-            address_id: number;
-            major_id: number;
-            study_plan_id: number;
-            semester: number;
-            birthday: Date;
-            student_state: StudentState;
-            tutor_id: number}, context: GraphQLContext) => {  
-                return context.prisma.student.update({
-                    where:{
-                        control:args.control,
-                    },
-                    data:{
-                        control: args.control,
-                        name: args.name,
-                        lastname1: args.lastname1,
-                        lastname2: args.lastname2,
-                        gender: args.gender,
-                        email: args.email,
-                        address_id: args.address_id,
-                        major_id: args.major_id,
-                        study_plan_id: args.study_plan_id,
-                        semester: args.semester,
-                        birthday: new Date(args.birthday),
-                        student_state: args.student_state,
-                        tutor_id: args.tutor_id  
-                    }
-                })
-        }
-    }
-}
+      });
+    },
+    updateStudent: async (
+      parent: Student,
+      args: {
+        control: string;
+        name: string;
+        lastname1: string;
+        lastname2: string;
+        gender: Gender;
+        email: string;
+        address_id: number;
+        major_id: number;
+        study_plan_id: number;
+        semester: number;
+        birthday: Date;
+        student_state: StudentState;
+        tutor_id: number;
+      },
+      context: GraphQLContext
+    ) => {
+      return context.prisma.student.update({
+        where: {
+          control: args.control,
+        },
+        data: {
+          control: args.control,
+          name: args.name,
+          lastname1: args.lastname1,
+          lastname2: args.lastname2,
+          gender: args.gender,
+          email: args.email,
+          address_id: args.address_id,
+          major_id: args.major_id,
+          study_plan_id: args.study_plan_id,
+          semester: args.semester,
+          birthday: new Date(args.birthday),
+          student_state: args.student_state,
+          tutor_id: args.tutor_id,
+        },
+      });
+    },
+  },
+};
--- src/graphql/tutor.ts
+++ src/graphql/tutor.ts
@@ -1,59 +1,64 @@
 import { Tutor } from "@prisma/client";
 import { GraphQLContext } from "../context";
-import { Context } from "vm";
 
 export const typeDefs = /* GraphQL */ `
-    type Tutor{
-        id:ID!
-        name:String
-    }
+  type Tutor {
+    id: ID!
+    name: String
+  }
 
-    extend type Query{
-        getAllTutors:[Tutor]
-    }
-    extend type Mutation{
-        createTutor(name:String):Tutor
-        deleteTutor(id:Int):Tutor
-        updateTutor(id:Int, name:String):Tutor
-    }
+  extend type Query {
+    getAllTutors: [Tutor]
+  }
+  extend type Mutation {
+    createTutor(name: String): Tutor
+    deleteTutor(id: Int): Tutor
+    updateTutor(id: Int, name: String): Tutor
+  }
 `;
 
 export const resolvers = {
-    Query: {
-        getAllTutors: async (parent: Tutor, args: {}, context: GraphQLContext) => {
-            return context.prisma.tutor.findMany();
-        },
+  Query: {
+    getAllTutors: async (parent: Tutor, args: {}, context: GraphQLContext) => {
+      return context.prisma.tutor.findMany();
     },
-    Mutation: {
-        createTutor: async (parent: Tutor,
-            args: { name: string },
-            context: GraphQLContext) => {
-            return context.prisma.tutor.create({
-                data: {
-                    name: args.name,
-                }
-            })
+  },
+  Mutation: {
+    createTutor: async (
+      parent: Tutor,
+      args: { name: string },
+      context: GraphQLContext
+    ) => {
+      return context.prisma.tutor.create({
+        data: {
+          name: args.name,
         },
-        deleteTutor: async (parent: Tutor,
-            args: { id: number },
-            context: GraphQLContext) => {
-            return context.prisma.tutor.delete({
-                where: {
-                    id: args.id,
-                }
-            })
+      });
+    },
+    deleteTutor: async (
+      parent: Tutor,
+      args: { id: number },
+      context: GraphQLContext
+    ) => {
+      return context.prisma.tutor.delete({
+        where: {
+          id: args.id,
         },
-        updateTutor:async (parent:Tutor, 
-            args:{id: number, name:string},
-            context:GraphQLContext) => {
-            return context.prisma.tutor.update({
-                where:{
-                    id:args.id,
-                },
-                data:{
-                    name:args.name,
-                }
-            })
-        }
-    }
-}
+      });
+    },
+    updateTutor: async (
+      parent: Tutor,
+      args: { id: number; name: string },
+      context: GraphQLContext
+    ) => {
+      return context.prisma.tutor.update({
+        where: {
+          id: args.id,
+        },
+        data: {
+          name: args.name,
+        },
+      });
+    },
+  },
+};
